416ccb873826dc6f9cb895cd3bb265bc6b3f35b3
codeinfo <nanye@codeinfo.cn>
2021-05-29 16:38:24 +0800

    fix redirect

--- src/Platform/Account/Account.php
+++ src/Platform/Account/Account.php
@@ -55,7 +55,7 @@ class Account extends Client
      *
      * @return void
      */
-    public function getRedriectUrl(string $url)
+    public function getRedirectUrl(string $url)
     {
         $response = $this->httpGetRedirect($url);
 
