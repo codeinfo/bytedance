15422ca928438afe34ff54559b9812cc067e3cea
ethan927 <chengguangxin927@gmail.com>
2022-03-15 09:42:01 +0800

    Update ServiceProvider.php

--- src/Platform/Image/ServiceProvider.php
+++ src/Platform/Image/ServiceProvider.php
@@ -12,7 +12,7 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $app)
     {
-        $app['video'] = function ($app) {
+        $app['image'] = function ($app) {
             return new Image($app);
         };
     }
