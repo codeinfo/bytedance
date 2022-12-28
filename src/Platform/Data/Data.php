805fd9ee1cd686d944356254eb733b190775db6e
cc <chengguangxin927@sina.com>
2022-12-28 09:14:21 +0800

    新增用户数据接口

--- src/Platform/Data/Data.php
+++ src/Platform/Data/Data.php
@@ -16,4 +16,46 @@ class Data extends Client
 
         return json_decode($response->getBody()->getContents(), true);
     }
+
+    public function userFans($access_token, $open_id, $date_type = 7)
+    {
+        $response = $this->httpGet('/data/external/user/fans/', ['open_id' => $open_id, 'date_type' => $date_type], ['access-token' => $access_token]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function userLike($access_token, $open_id, $date_type = 7)
+    {
+        $response = $this->httpGet('/data/external/user/like/', ['open_id' => $open_id, 'date_type' => $date_type], ['access-token' => $access_token]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function userComment($access_token, $open_id, $date_type = 7)
+    {
+        $response = $this->httpGet('/data/external/user/comment/', ['open_id' => $open_id, 'date_type' => $date_type], ['access-token' => $access_token]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function userShare($access_token, $open_id, $date_type = 7)
+    {
+        $response = $this->httpGet('/data/external/user/share/', ['open_id' => $open_id, 'date_type' => $date_type], ['access-token' => $access_token]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function userProfile($access_token, $open_id, $date_type = 7)
+    {
+        $response = $this->httpGet('/data/external/user/profile/', ['open_id' => $open_id, 'date_type' => $date_type], ['access-token' => $access_token]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function userItem($access_token, $open_id, $date_type = 7)
+    {
+        $response = $this->httpGet('/data/external/user/item/', ['open_id' => $open_id, 'date_type' => $date_type], ['access-token' => $access_token]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
 }
